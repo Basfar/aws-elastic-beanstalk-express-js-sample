da4541b6f79ffc827176f2b4dba5801fe795457b
basfar <mukhtaronif@gmail.com>
2022-06-26 00:24:22 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Mukhtar. You the best!'));
+app.get('/', (req, res) => res.send('Heloo Mukht babs!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
